681d459bcbb6ec93cf29151d6de2659939259b04
larwon <74400003+larwon@users.noreply.github.com>
2021-03-11 21:24:43 +0900

    Add files via upload

---
+++ 07.file_upload.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form name="test" method="post" action="07.view8.php"
+            enctype="multipart/form=data">
+            <ul>
+                <li>이미지 업로드 : <input type = "file" name = "upload"></li>
+                <li>설명 : <textarea name="comment" rows="3", cols="60"></textarea></li>
+                
+            </ul>
+            <input type="submit" value="확인">         
+        </form>
+    </body>
+</html>

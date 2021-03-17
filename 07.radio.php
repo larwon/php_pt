c9b8f7493935d60323fdd1f9f1e24cf208fedbe7
larwon <74400003+larwon@users.noreply.github.com>
2021-03-17 20:28:33 +0900

    Add files via upload

---
+++ 07.radio.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form name="test" method="post" action="07.view4.php">
+            <ul>
+                <li>성별 : 남성 <input type="radio" name="gender" value="남" checked>
+                    여성 <input type="radio" name="gender" value="여">
+                </li>
+                <li>이메일 : 수신 <input type="radio" name="email_ok" value="예">
+                    비수신 <input type="radio" name="email_ok" value="아니오" checked>
+                </li>
+
+            </ul>    
+        <input type="submit" value="확인">
+        </form>
+    </body>
+</html>
